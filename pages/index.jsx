c287c6e41418a79551c179df55516a0d622afc78
Matt-htims <matt.smith3108@gmail.com>
2021-11-29 21:23:56 +0000

    Finished styling of person section now onto grid - trouble with grid resizing

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,49 +14,56 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main>
+			<main className={styles.main}>
 				<div className={styles.person}>
 					<div className={styles.image}>
-						<Image
-							src="/profilePhoto.png"
-							width="156"
-							height="156"
-							alt="Profile photo"
-						/>
+						<Image src="/profilePhoto.png" layout="fill" alt="Profile photo" />
 					</div>
 					<h2 className={styles.heading}>Rodney Cotton</h2>
 					<p className={styles.subheading}>Helsinki, Finland</p>
 					<div className={styles.stats}>
 						<div className={styles.statbox}>
 							<p>100</p>
-							<p>Posts</p>
+							<h4>Posts</h4>
 						</div>
 						<div className={styles.statbox}>
 							<p>2,242</p>
-							<p>Follower</p>
+							<h4>Follower</h4>
 						</div>
 						<div className={styles.statbox}>
 							<p>1,432</p>
-							<p>Following</p>
+							<h4>Following</h4>
 						</div>
 					</div>
 				</div>
-				{/* <div className={styles.gallery}>
-					<Image src="/photo1.png" layout="fill" alt="Stool in room" />
-					<Image src="/photo2.png" layout="fill" alt="Sofa in room" />
-					<Image src="/photo3.png" layout="fill" alt="Standing bath tub" />
-					<Image
-						src="/photo4.png"
-						layout="fill"
-						alt="Fancy black standing lamp"
-					/>
-					<Image src="/photo5.png" layout="fill" alt="Green exotic plant" />
-					<Image
-						src="/photo6.png"
-						layout="fill"
-						alt="Farm house table with low wicker stools"
-					/>
-				</div> */}
+				<div className={styles.gallery}>
+					<div className={styles.galleryimage}>
+						<Image src="/photo1.png" layout="fill" alt="Stool in room" />
+					</div>
+					<div className={styles.galleryimage}>
+						<Image src="/photo2.png" layout="fill" alt="Sofa in room" />
+					</div>
+					<div className={styles.galleryimage}>
+						<Image src="/photo3.png" layout="fill" alt="Standing bath tub" />
+					</div>
+					<div className={styles.galleryimage}>
+						<Image
+							src="/photo4.png"
+							layout="fill"
+							alt="Fancy black standing lamp"
+						/>
+					</div>
+					<div className={styles.galleryimage}>
+						<Image src="/photo5.png" layout="fill" alt="Green exotic plant" />
+					</div>
+					<div className={styles.galleryimage}>
+						<Image
+							src="/photo6.png"
+							layout="fill"
+							alt="Farm house table with low wicker stools"
+						/>
+					</div>
+				</div>
 			</main>
 			<footer>
 				<p>
